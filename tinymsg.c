6bb158f8006a0856cbb4f092615603aee6381208
Christopher R. Johnson <crjohns@csail.mit.edu>
2013-05-22 22:22:12 -0400

    Fixed up initialization code

--- tinymsg.c
+++ tinymsg.c
@@ -12,27 +12,60 @@ int name_fd;
 tm_name_header *name_table = 0;
 tm_name_entry *my_name_entry;
 
-static void format_name(unsigned int name, char *cname, int maxlen)
+tm_mailbox *my_mailbox, *my_dataBox, *my_freeBox;
+
+static void format_name(uint32_t name, char *cname, const char *suffix, int maxlen)
 {
-    snprintf(cname, maxlen, "/%x", name);
+    snprintf(cname, maxlen, "/%x%s", name, suffix);
+}
+
+static uint32_t rand_name()
+{
+    return rand() % 0xFFFFFFFF;
+}
+
+static void *create_shared_mapping(const char *name)
+{
+    int fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL);
+    if(fd < 0)
+    {
+        perror("shm open");
+        abort();
+    }
+
+    ftruncate(fd, MAPPING_LEN);
+    void *ret = mmap(0, MAPPING_LEN, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if(ret == MAP_FAILED)
+    {
+        perror("mmap");
+        abort();
+    }
+
+    return ret;
 }
 
 int tm_init(unsigned long name)
 {
     char cname[128];
 
-    format_name(name, cname, 128);
 
     if(!name_table)
     {
-        name_fd = shm_open("/tm_names", O_RDWR | O_CREAT);
+        name_fd = shm_open("/tm_names", O_RDWR);
+        if(name_fd < 0 && errno == ENOENT)
+        {
+            // Create the shm
+            name_fd = shm_open("/tm_names", O_RDWR | O_CREAT | O_EXCL);
+            if(name_fd >= 0)
+                ftruncate(name_fd, MAPPING_LEN);
+        }
+
         if(name_fd < 0)
         {
             fprintf(stderr, "Failed to open shm\n");
             return errno;
         }
 
-        ftruncate(name_fd, MAPPING_LEN);
 
         name_table = (tm_name_header*) mmap(0, MAPPING_LEN, PROT_READ | PROT_WRITE, MAP_SHARED,
                 name_fd, 0);
@@ -51,6 +84,14 @@ int tm_init(unsigned long name)
         return -1;
     }
 
+
+    format_name(name, cname, "_mb", 128);
+    my_mailbox = (tm_mailbox*)create_shared_mapping(cname);
+    format_name(name, cname, "_data", 128);
+    my_dataBox= (tm_mailbox*)create_shared_mapping(cname);
+    format_name(name, cname, "_free", 128);
+    my_freeBox = (tm_mailbox*)create_shared_mapping(cname);
+
     my_name_entry = &name_table->entries[myslot];
     my_name_entry->name = name;
     __sync_synchronize();
@@ -64,8 +105,20 @@ int tm_init(unsigned long name)
 void tm_cleanup(void)
 {
     char cname[128];
-    format_name(my_name_entry->name, cname, 128);
+
+
+    munmap(my_mailbox, MAPPING_LEN);
+    munmap(my_freeBox, MAPPING_LEN);
+    munmap(my_dataBox, MAPPING_LEN);
+
+    format_name(my_name_entry->name, cname, "_mb", 128);
+    shm_unlink(cname);
+    format_name(my_name_entry->name, cname, "_data", 128);
+    shm_unlink(cname);
+    format_name(my_name_entry->name, cname, "_free", 128);
+    shm_unlink(cname);
     my_name_entry->valid = 0;
+
     munmap(name_table, MAPPING_LEN);
     shm_unlink(cname);
 }
--- tinymsg.h
+++ tinymsg.h
@@ -1,6 +1,8 @@
 #ifndef TINYMSG_H
 #define TINYMSG_H
 
+#include <stdint.h>
+
 #ifndef CACHELINE
 #define CACHELINE 64
 #endif
@@ -8,26 +10,56 @@
 #define NAME_ENTRIES 1024
 #define NAME_PAGE_LEN (NAME_ENTRIES * sizeof(struct tm_name_entry) + sizeof(struct tm_name_header))
 
+#define MESSAGE_SLOTS 4096
+
+#define PASTE(x,y) x ## y
+#define PASTE2(x,y) PASTE(x,y)
 #define __mpalign__ __attribute__((aligned(CACHELINE)))
-#define __padout__ char pad##__LINE__[0] __mpalign__
+#define __padout__ char PASTE2(pad, __COUNTER__)[0] __mpalign__
 
 typedef struct
 {
-    unsigned long name;
-    unsigned long valid;
-} tm_name_entry;
+    uint32_t name;
+    uint32_t valid;
+    __padout__;
+} tm_name_entry __mpalign__;
 
 typedef struct 
 {
-    unsigned int curslot;
+    uint32_t curslot;
     __padout__;
     
-
     tm_name_entry entries[0];
 } tm_name_header  __mpalign__;
 
-void tm_cleanup(void);
+typedef struct 
+{
+    union
+    {
+        struct
+        {
+            uint32_t datamap;
+            uint32_t slot;
+        };
+        uint64_t message;
+    };
+} tm_message;
 
+#define MAKE_MESSAGE(datamap, slot) ((datamap << 32) | slot)
+
+typedef struct
+{
+    unsigned long head __mpalign__;
+    __padout__;
+    unsigned long tail __mpalign__;
+    __padout__;
+
+    tm_message slots[0];
+
+} tm_mailbox;
+
+
+void tm_cleanup(void);
 int tm_init(unsigned long name);
 
 
